89bafd272009d2c0acada0cedefe1dd6bed40f32
omerahmer <omerahmer918@gmail.com>
2022-10-21 16:07:51 -0700

    Finished structures assignment

--- structures/a7.cpp
+++ structures/a7.cpp
@@ -12,8 +12,7 @@ void inputData(string filename, SoccerPlayer players[]);
 void display(SoccerPlayer players[]);
 void sort(SoccerPlayer players[]);
 void swapping(SoccerPlayer&, SoccerPlayer&);
-int playerSearch(SoccerPlayer players[], char[]);
-void find(SoccerPlayer players[]);
+int playerSearch(SoccerPlayer players[]);
 const int PLAYERS_SIZE = 10;
 
 int main() {
@@ -41,10 +40,7 @@ int main() {
             case 4:
                 display(copyPlayers);
             case 5:
-                char input[21];
-                cout << "Enter first few characters of player you want to find: " << endl;
-                cin >> input;
-                int index = playerSearch(players, input);
+                int index = playerSearch(players);
         }
     } while (option != 6);
 }
@@ -94,4 +90,56 @@ void sort(SoccerPlayer players[]) {
         } 
     }
     display(players);
+}
+
+void swapping(SoccerPlayer &player1, SoccerPlayer &player2) {
+    char temp_array[20];
+
+    strcpy(temp_array, player1.first);
+    strcpy(player1.first, player2.first);
+    strcpy(player2.first, temp_array);
+
+    strcpy(temp_array, player1.last);
+    strcpy(player1.last, player2.last);
+    strcpy(player2.last, temp_array);
+
+    int tempday = player1.day;
+    player1.day = player2.day;
+    player2.day = tempday;
+
+    int tempmonth = player1.month;
+    player1.month = player2.month;
+    player2.month = tempmonth;
+
+    int tempyear = player1.year;
+    player1.year = player2.year;
+    player2.year = tempyear;
+
+}
+
+int playerSearch(SoccerPlayer players[]) {
+    string last = "";
+    while (last != "exit") {
+        int found = 1;
+        int flag = 0;
+        char input[21];
+        cout << "Enter first few characters of player you want to find: " << endl;
+        cin >> input;
+
+        for (int i = 0; i < PLAYERS_SIZE; i++) {
+            for (int j = 0; j < last.length(); j++) {
+                if (players[i].last[j] != last.at(j)) {
+                    found = 0;
+                    break;
+                }
+            }
+            if (found == 1) {
+                flag = 1;
+                display(players);
+            }
+        }
+        if (flag == 0) {
+            cout << "Not found." << endl;
+        }
+    }
 }
